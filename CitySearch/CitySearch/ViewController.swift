fd39d1b6d680c6761ef88a327cad9502c2e1811f
usman <usmanahmedsiddiqui@icloud.com>
2018-10-19 14:11:51 -0400

    Initial Project setup

---
+++ CitySearch/CitySearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CitySearch
+//
+//  Created by Usman Siddiqui on 10/19/18.
+//  Copyright © 2018 Usman_Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
